c529b3b976fc48d4aa593de6f6a948c45571fe3d
zhaozixin <zhao.zixin@northeastern.edu>
2021-12-11 06:12:07 -0800

    test

--- app/src/main/java/edu/neu/madcourse/thingshub/FrontEnd/FriendActivity.java
+++ app/src/main/java/edu/neu/madcourse/thingshub/FrontEnd/FriendActivity.java
@@ -40,7 +40,7 @@ public class FriendActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-    @Override
+
 
     private void init(Bundle savedInstanceState) {
         initialItemData(savedInstanceState);
